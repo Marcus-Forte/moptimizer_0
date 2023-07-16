c9018fcca039be0964c6bfcd6a717bc33450f408
Marcus Forte <marcus.forte@dunasystem.com>
2023-07-16 23:34:15 +0200

    improved cmake

--- formatter.sh
+++ formatter.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "Formatting code..."
-find . \( -iname *.h -o -iname *.cpp -o -iname *.md \) -and -not -iname *duna_exports.h | xargs clang-format-16 -i
+find src tst include \( -iname *.h -o -iname *.cpp -o -iname *.md \) -and -not -iname *duna_exports.h | xargs clang-format-16 -i
