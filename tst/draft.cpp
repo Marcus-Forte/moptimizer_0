33d117f17dd3c34ac6ecfa81c6d7ac25bbc92e4d
Marcus Forte <marcus.forte@dunasystem.com>
2023-12-10 16:08:42 +0100

    draft

--- tst/draft.cpp
+++ tst/draft.cpp
@@ -2,23 +2,38 @@
 #include <duna_optimizer/logger.h>
 
 #include <Eigen/Dense>
+#include <duna_optimizer/stopwatch.hpp>
 #include <fstream>
 #include <iostream>
 /// General sandbox program.
 
-void fun(const Eigen::Ref<Eigen::Matrix<double, 3,3>>& m) {
-  std::cout << m << std::endl;
-}
+void fun(const Eigen::Ref<Eigen::Matrix<double, 3, 3>>& m) { std::cout << m << std::endl; }
 
 int main(int argc, char** argv) {
-  Eigen::Matrix<double, 3, 3> jac_transpose;
+  int size = 50;
+  // VectorXf is a vector of floats, with dynamic size.
+  Eigen::VectorXf u(size), v(size), w(size);
+  u = v + w;
+  // Eigen::Matrix<double, -1, -1> matrix;
+  // Eigen::Matrix<double, -1, -1> matrixb;
+  // int dim = 1000;
 
-  jac_transpose = Eigen::Matrix<double,3,3>::Random(2,2);
+  // matrix.setRandom(dim, dim);
+  // matrixb.setRandom(dim, dim);
+  // utilities::Stopwatch watch;
+  // watch.enable();
+  // watch.tick();
+  // auto res = matrix * matrixb;
+  // watch.tock("multiplication\n");
 
-  Eigen::Map<Eigen::Matrix<double, 3,3>> some_map(nullptr);
-  
-  new(&some_map) Eigen::Map<Eigen::Matrix<double,3,3>>(jac_transpose.data(), 3,3);
-  some_map(1,1) = 100;
+  // std::cout << "Maaping mult...\n";
+  // Eigen::Map<Eigen::Matrix<double, -1, -1>> mapA(matrix.data(), dim, dim);
+  // Eigen::Map<Eigen::Matrix<double, -1, -1>> mapB(matrixb.data(), dim, dim);
+  // Eigen::Matrix<double, -1, -1> resmap;
+  // resmap = mapA * mapB;
 
-  fun(jac_transpose);
+  // std::cout << "Done\n";
+  // std::cout << resmap(50,50) << std::endl;
+
+  // std::cout << res(50,50);
 }
